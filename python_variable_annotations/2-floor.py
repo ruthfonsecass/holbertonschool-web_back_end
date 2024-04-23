57cf34848f560f681b45f2db194eb486ac40dd8d
ruthfonsecass <7502@holbertonstudents.com>
2024-04-23 15:38:36 +0200

    files

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import math
+
+def floor(n: float) -> int:
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def to_sntr(n: float) -> str:
+    return str(n)
