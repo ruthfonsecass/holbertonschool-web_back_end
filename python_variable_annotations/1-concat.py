5f6319852ed82409d536ea85f4c361bd5e8f3aac
ruthfonsecass <7502@holbertonstudents.com>
2024-04-24 22:16:17 +0200

    update

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-
+"""
+    oncat that takes a string str1 and a
+    string str2 as arguments
+"""
 def concat(str1: str, str2: str) -> str:
+    """
+        returns a concatenated string
+    """
     return str1 + str2
