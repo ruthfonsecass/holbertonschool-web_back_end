38b1f4cfe2f39f9bdc5ea2d0b7a7c94221c874ad
ruthfonsecass <7502@holbertonstudents.com>
2024-04-24 14:20:56 +0200

    update

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
 """
-function add that takes a float a
-and a float b as arguments and
-returns their sum as a float.
+    function add that takes a float a
+    and a float b as arguments and
+    returns their sum as a float.
 """
 
 from typing import List
