69a194da388980be3499ac60058dc6f7351239e6
ruthfonsecass <7502@holbertonstudents.com>
2024-04-24 22:14:13 +0200

    update

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+    function add that takes a float a
+    and a float b as arguments
+"""
+
 
 def add(a: float, b: float) -> float:
+    """
+        returns their sum as a float.
+    """
     return a + b
